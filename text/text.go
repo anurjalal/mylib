bf458c92de5597a1f3fc78e204ce4f9ac9d43cee
Ahmad Nurjalal <ahmad.nurjalal@accelbyte.net>
2021-10-21 09:30:23 +0700

    second commit

--- text/text.go
+++ text/text.go
@@ -1,9 +1,16 @@
 package text
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 type Name struct {
 	MyName string
 }
 
 func (name Name) name() string  {
+	log.Infof("this is info")
 	return "this is v1.0.0"
+
 }
+
